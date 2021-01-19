6ae14d0e1f51da30c7b0a6dec1a28a0482cb350d
Philip Pérez Castro <jperez.c92@gmail.com>
2021-01-18 22:57:30 -0500

    app finished

---
+++ src/GifExpertApp.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
+
+const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category, index) => (
+          <GifGrid key={category + index} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GifExpertApp;
